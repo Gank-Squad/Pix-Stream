b8963b794d030e666f297b8d754c2e775eb29970
ziridia <90412489+ziridia@users.noreply.github.com>
2023-03-14 13:40:48 -0400

    working on search - can't finish rn

--- FinalProject/src/main/java/uwu/nyaa/owo/finalproject/APIResource.java
+++ FinalProject/src/main/java/uwu/nyaa/owo/finalproject/APIResource.java
@@ -3,9 +3,13 @@ package uwu.nyaa.owo.finalproject;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.net.URLEncoder;
+import java.net.URLDecoder;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.Consumes;
@@ -80,4 +84,51 @@ public class APIResource
         return Response.status(200).build();
     }
 
+    /*
+
+    terms should be a string of tags, with each tag separated by a delimiter.
+    "&" or %26 will probably work. Strings passed by frontend should be encoded, but an additional check
+    on backend is also probably a good idea
+
+    Gets search results from querying the db, returns as a json with (start-end) elements
+     */
+    @GET
+    @Produces("application/json")
+    @Path("/search/{tags}/{start}-{end}")
+    public Response getSearchResults(@PathParam("tags") String us_tags, @PathParam("start") String start, @PathParam("end") String end)
+    {
+        // I'm not really sure how to query the database, and don't have time to find out
+        // Want to avoid sending like 10k things to user at once, so requests should be done in *relatively* small increments
+
+        // all the following stuff probably doesn't matter - I know almost nothing about web security
+        // check if url is encoded (by decoding and comparing)
+        String s_tags;
+        try
+        {
+            if (us_tags.equals(URLDecoder.decode(us_tags, StandardCharsets.UTF_8.toString())))
+            {
+                // not encoded
+                s_tags = URLEncoder.encode(us_tags, StandardCharsets.UTF_8.toString());
+            }
+            else
+            {
+                // it is encoded
+                s_tags = us_tags;
+            }
+        } catch (UnsupportedEncodingException e)
+        {
+            return Response.status(500).entity("{}").build();
+        }
+
+        // if it differs, encode the uri
+
+        // with known to be encoded uri, split by delimiter
+        String tags[] = s_tags.split("%26");
+        // query db, store results as a json, and return
+
+        // if theres an invalid tag, just return an empty json object
+        // if start or end are invalid, probably return an error status
+
+        return Response.status(200).entity("abc").build();
+    }
 }
