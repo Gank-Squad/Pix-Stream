b3bbde49e167d02d81e0fe0ba1ce2c3aaaa5a99d
AliceNyaa <90223538+AliceNyaa@users.noreply.github.com>
2023-03-28 20:29:54 -0400

    fixed the fuckked sidepanel js

--- frontEnd/final_project/src/components/elements/TagSidebar.js
+++ frontEnd/final_project/src/components/elements/TagSidebar.js
@@ -1,14 +1,14 @@
 
 import React from "react";
 
-export default function VideoPlayer(props) 
-{
+export default function TagSidebar(props) {
+    // basically global variables / instance variables 
     const [search, setSearch] = React.useState([]);
-    const [tags, setTags] = React.useState({});
-    const [tagDisplay, setTagDisplay] = React.useState(null);
+    const [tags, setTags] = React.useState(null);
 
     const TAGS_API = "http://192.168.1.148:8080/FinalProject-1.0-SNAPSHOT/api/tags";
 
+    // html elements that we will be using 
     const tagSearch = React.useRef("");
     const selectedTagBox = React.useRef("");
 
@@ -17,14 +17,32 @@ export default function VideoPlayer(props)
 
     const contextTagBox = React.useRef("");
 
-    function clearSelected() 
-    {
+    React.useEffect(() => {
+
+        fetch(TAGS_API, { 'method': 'get' })
+            .then(response => response.json())
+            .then(json => {
+
+                setTags(json);
+            })
+            .catch(e => console.log(e));
+
+    }, []);
+
+
+    if (tags == null) {
+        return <div>Loading...</div>;
+    }
+
+    // standard functions 
+    function clearSelected() {
+        // selectedTagBox.current is the DOM element, selectedTagBox is the react object
+        // selectedTagBox.current can be used like normal js
         const contextTags = selectedTagBox.current.querySelectorAll('tr');
 
         const tbody = contextTagBox.current.querySelector('tbody');
 
-        contextTags.forEach(element => 
-            {
+        contextTags.forEach(element => {
             tbody.appendChild(element);
         });
     }
@@ -47,13 +65,10 @@ export default function VideoPlayer(props)
         });
     }
 
-    function updateSearch(tag_id, tag)
-    {
-        console.log("updating search with tag id: " + tag_id + " (not really this function does nothing you need to finish it)");
+    function updateSearch(tag) {
+        console.log("updating search with tag id: " + JSON.stringify(tag) + " (not really this function does nothing you need to finish it)");
 
-        setSearch(prevItems => [...prevItems, tag_id]);
-
-        console.log(search);
+        setSearch(prevItems => [...prevItems, tag]);
     }
 
     function searchValueKeyDown(e) {
@@ -68,71 +83,28 @@ export default function VideoPlayer(props)
         filterTags(tagSearch.current.value);
     }
 
-    function tagButtonPressed(e) {
+    function tagButtonPressed(e, tag) {
         if (!e || !e.target)
             return;
 
-        let tag_id = e.target.getAttribute('data-key');
-
-        const trSelected = selectedTagBox.current.querySelector(`tr[data-key="${tag_id}"]`);
-        const trContext = contextTagBox.current.querySelector(`tr[data-key="${tag_id}"]`);
+        const trSelected = selectedTagBox.current.querySelector(`tr[data-key="${tag.tag_id}"]`);
+        const trContext = contextTagBox.current.querySelector(`tr[data-key="${tag.tag_id}"]`);
 
         if (trSelected) {
             contextTagBox.current.querySelector('tbody').appendChild(trSelected);
         }
         else {
             selectedTagBox.current.querySelector('tbody').appendChild(trContext);
-
-            // idk why this doesn't work all the time
-            updateSearch(tag_id, tags[tag_id]);
+            updateSearch(tag);
         }
     }
-
-
-    React.useEffect(() => {
-        fetch(TAGS_API, { 'method': 'get' })
-            .then(response => response.json())
-            .then(json => {
-
-                let mappedTags = {};
-
-                console.log(json);
-
-                let tagDisp = [];
-                for (let tag of json) 
-                {
-                    mappedTags[tag.tag_id] = tag; 
-
-                    tagDisp.push(
-                        <tr key={tag.tag_id} data-key={tag.tag_id}>
-                            <td>
-                                <div className="tag">
-                                    <i className="fa fa-tag"></i>
-                                    <button data-key={tag.tag_id} onClick={tagButtonPressed} className="fa fa-plus">+</button>
-                                    <label >
-                                        {tag.namespace + ":" + tag.subtag}
-                                    </label>
-                                </div>
-                            </td>
-                        </tr>
-                    );
-                }
-
-                setTags(mappedTags);
-
-                setTagDisplay(tagDisp);
-            })
-            .catch(e => console.log(e));
-
-    }, []);
-
 
 
     return (
 
         <div id="navbar-background" >
 
-            {search}
+            {search.map((x) => JSON.stringify(x))}
 
             <div id="navbar-content">
 
@@ -168,7 +140,22 @@ export default function VideoPlayer(props)
 
                     <table ref={contextTagBox} className="tagbox">
                         <tbody>
-                            {tagDisplay}
+
+
+                            {tags.map(tag => {
+                                return <tr key={tag.tag_id} data-key={tag.tag_id}>
+                                    <td>
+                                        <div className="tag">
+                                            <i className="fa fa-tag"></i>
+                                            <button data-key={tag.tag_id} onClick={(e) => tagButtonPressed(e, tag)} className="fa fa-plus">+</button>
+                                            <label >
+                                                {tag.namespace + ":" + tag.subtag}
+                                            </label>
+                                        </div>
+                                    </td>
+                                </tr>
+                            })}
+
                         </tbody>
                     </table>
                 </div>
