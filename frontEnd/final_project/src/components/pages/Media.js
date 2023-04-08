61717bedd361da1b77f5f1e660bdddddffc4a8e8
ziridia <90412489+ziridia@users.noreply.github.com>
2023-04-07 23:28:23 -0400

    ui changes to media page

--- frontEnd/final_project/src/components/pages/Media.js
+++ frontEnd/final_project/src/components/pages/Media.js
@@ -42,9 +42,6 @@ export default function Default(props)
         }
         
 
-        // hardcoding the hash for testing purposes
-        hash = "218af290730ee6760f45f2b8c4e0caf00e92cf82932e933442b791d8f876fe02";
-
         const url = API_ENDPOINTS.media.get_file + hash;
 
         fetch(url, {method:"GET"}).then(resp =>
@@ -52,7 +49,9 @@ export default function Default(props)
                 if (resp.status === 200)
                 {
                     console.log("loading media page for hash: " + hash);
-                    return resp.json();
+                    let temp = resp.json();
+                    console.count("RESPONSE:" + temp)
+                    return temp;
                 }
                 else
                 {
@@ -61,7 +60,9 @@ export default function Default(props)
                 }
             }
         ).then(dataJson => {
+            console.count("before:"+dataJson);
             setMediaData(dataJson);
+            console.count("after");
         }).catch(err => {
             if (err === "server") return
             console.log(err)
@@ -150,20 +151,22 @@ export default function Default(props)
             <nav ref={sidebar}
                 className="group fixed top-20 left-0 h-screen w-60 -translate-x-60 overflow-y-auto overflow-x-hidden shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:translate-x-0 bg-custom-dark-blue"
                 data-te-sidenav-init
-                data-te-sidenav-hidden="true"
+                data-te-sidenav-hidden="false"
             >
                 <ul className="relative m-0 list-none px-[0.2rem]" data-te-sidenav-menu-ref>
                     <TagSidebar {...tagSidebarProps} />
                 </ul>
             </nav>
 
-
-            <header className="w-full p-4 h-20 bg-custom-dark-blue text-left absolute">
-                <div>
+            <header className="w-full p-4 h-20 bg-custom-dark-blue absolute">
+                <table>
+                    <tbody>
+                    <tr>
+                <td><div className="text-left inline-block">
                     {/* The following is the code for the hamburger menu
                     but I'm not really sure how to make it show up inline with everything else */}
                     <button
-                        className="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 border border-blue-700 rounded-l"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-l"
                         data-te-sidenav-toggle-ref
                         aria-haspopup="true"
                         onClick={toggleSidebarVisibility}
@@ -182,19 +185,25 @@ export default function Default(props)
                         </span>
                     </button>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-l" onClick={redirect_home}>Home</button>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 border border-blue-700 px-4" onClick={redirect_tags}>Tags</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 border border-blue-700 px-4" disabled onClick={redirect_tags}>Tags</button>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-r" onClick={redirect_upload}>Upload</button>
-                </div>
+                </div></td>
+
+                <td className="center"><div className="text-center inline"><p className="text-xl font-bold">PixStream</p></div></td>
+                </tr></tbody>
+                </table>
             </header>
 
-            <main className="flex-1 overflow-y-auto px-80 py-12" ref={main}>
+            <main className="overflow-y-auto py-12 px-72" ref={main}>
             {/* PUT ALL DISPLAY STUFF IN HERE, ANYTHING OUTSIDE MAY NOT BE FORMATED CORRECTLY */}
-
-                <p>Hello world, you are on the media page {(page).toString()}</p>
-                {mediaData.map((json, index) => 
+            {
+                console.log("mediaData")
+            }
+            {mediaData.map((json, index) => 
                 {
+                    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
                     const props = {
-                        "image" : formatStringB(API_TEMPLATES.get_file.url, json.hash),
+                        "image" : formatStringB(API_TEMPLATES.get_file.url, hash),
                         "hlsUrl": formatStringB(API_TEMPLATES.get_file.url, json.hash),
                         "caption": json.mime,
                         "style" : {
@@ -214,7 +223,10 @@ export default function Default(props)
                             // "justify-content": "center"
                         }
                     }
-
+                    
+                    
+                    
+                    console.log(json.hash);
                     if(json.mime_int >= 20)
                     {
                         return <VideoPlayer key={index} {...props}></VideoPlayer>;
@@ -222,7 +234,10 @@ export default function Default(props)
                     
                     return <ImageContainer key={index} {...props} imgError={e => console.log("image errr")}></ImageContainer>;
                 })}
+            
             </main>
+
+
         </div>
     )
 }
