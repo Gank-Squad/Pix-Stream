59310c2f7f535e5b49381e83916d861a0e33d458
AliceNyaa <90223538+AliceNyaa@users.noreply.github.com>
2023-04-14 16:57:22 -0400

    fixed hamburger menu

--- frontEnd/final_project/src/components/elements/HeaderBar.js
+++ frontEnd/final_project/src/components/elements/HeaderBar.js
@@ -35,8 +35,8 @@ export default function HeaderBar({ toggleSidebarVisibility })
                         >
                         <span 
                         // className="block [&>svg]:h-5 [&>svg]:w-5 [&>svg]:text-white"
-                        >
-                            <svg
+                        >☰
+                            {/* <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 24 24"
                                 fill="currentColor"
@@ -46,11 +46,11 @@ export default function HeaderBar({ toggleSidebarVisibility })
                                     fillRule="evenodd"
                                     d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
                                     clipRule="evenodd" />
-                            </svg>
+                            </svg> */}
                         </span>
                     </button>}
                     {/* create buttons for home, tags, and upload redirect */}
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-l" onClick={redirect_home}>Home</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 " onClick={redirect_home}>Home</button>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 border border-blue-700 px-4" onClick={redirect_tags}>Tags</button>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-r" onClick={redirect_upload}>Upload</button>
             </header>
